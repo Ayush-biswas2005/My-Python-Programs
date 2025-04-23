f9ead8a6d7814cca9651f43013a4ec9af6c59db6
Ayush Biswas <ayushbiswascob@gmail.com>
2025-04-23 23:50:55 +0530

    Odd or even

---
+++ Programs/OddOrEven.py
@@ -0,0 +1,7 @@
+#** Even or Odd ** – Write a program that checks if a number is even or odd using conditional statements.
+
+num = int(input("Enter a number:"))
+if num % 2 == 0:
+    print(f"{num} is even.")
+else:
+    print(f"{num} is odd.")
